1a921b918c63938e6d5c80bfc3a34fa1a37bb51c
KhushiKhanal4 <khanalkhushi123@gmail.com>
2024-07-20 01:32:37 +0545

    Update hamburger menu

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-const hamburger=document.querySelector(".hamburger");
-const navBar=document.querySelector(".navBar");
+const hamburger = document.querySelector(".hamburger");
+const navBar = document.querySelector(".navBar");
+const navLinks = document.querySelectorAll(".navBar li a");
 
-hamburger.addEventListener("click",() => {
+hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navBar.classList.toggle("active");
-})
+});
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navBar.classList.remove("active");
+    });
+});
+
+
+
+
