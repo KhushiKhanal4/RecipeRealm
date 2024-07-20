06e09e128a266e93d1d674ea03e7e3ee3e0cad58
KhushiKhanal4 <khanalkhushi123@gmail.com>
2024-07-20 14:56:01 +0545

    Fetch API

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Hamburger Menu
+
 const hamburger = document.querySelector(".hamburger");
 const navBar = document.querySelector(".navBar");
 const navLinks = document.querySelectorAll(".navBar li a");
@@ -13,6 +15,33 @@ navLinks.forEach(link => {
         navBar.classList.remove("active");
     });
 });
+
+// Recipe Finder
+
+// findRecipe function
+
+async function findRecipe(query){
+    const apiKey='5a9d81283395453eb818b7f82e920114';
+    const endPoint=`https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=${apiKey}`;
+
+    try{
+        const response =await fetch(endPoint);
+        if(!response.ok)
+        {
+            throw new Error('Network response was not ok');
+        }
+
+        const output= await response.json();
+        console.log('API response:', output); 
+
+        viewResult(output.results);
+   }
+    catch (error) {
+        console.error('Problem in fetch operation', error);
+    }
+}
+
+//viewResults Function
 
 
 
